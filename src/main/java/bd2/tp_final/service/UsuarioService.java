9368d846e087d1e7fe59b177b69550d2ce814ff5
Gabriel Brañeiro <gabbraneiro@gmail.com>
2019-08-20 13:40:15 -0300

    -

--- src/main/java/bd2/tp_final/service/UsuarioService.java
+++ src/main/java/bd2/tp_final/service/UsuarioService.java
@@ -2,37 +2,33 @@ package bd2.tp_final.service;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
+import org.springframework.stereotype.Service;
 import bd2.tp_final.dto.Usuario;
-import bd2.tp_final.repository.UsuarioRepository;
+
 
 @Service
-public class UsuarioService {
+public class UsuarioService extends ProdeService{
 
-	@Autowired
-	private UsuarioRepository usuarioDAO;
-	
 	/* CONSULTA */
 	public Iterable<Usuario> obtenerUsuarios(){
-		return usuarioDAO.findAll();
+		return usuarioRepository.findAll();
 	}
 	
 	/* CONSULTA */
 	public Optional<Usuario> obtenerUsuario(Integer usuarioId){
-		return usuarioDAO.findById(usuarioId);
+		return usuarioRepository.findById(usuarioId);
 	}	
 	
 	/* ALTA */
 	public Usuario nuevoUsuario(String username, String nombre, String apellido) throws Exception {
-		Optional<Usuario> usuarioExistente = usuarioDAO.findByUsername(username);
+		Optional<Usuario> usuarioExistente = usuarioRepository.findByUsername(username);
 		if(usuarioExistente.isPresent()) {
 			throw new Exception("El username elegido ya existe, por favor elija otro");
 		}
 		Usuario u = new Usuario(username, nombre, apellido);
 		
-		return usuarioDAO.save(u);
+		return usuarioRepository.save(u);
 	}
 	
 	/* MODIFICACIÓN */
@@ -40,12 +36,12 @@ public class UsuarioService {
 		usuario.setUsername(username);
 		usuario.setNombre(nombre);
 		usuario.setApellido(apellido);		
-		return usuarioDAO.save(usuario);
+		return usuarioRepository.save(usuario);
 	}
 	
 	/* BAJA */
 	public void eliminarUsuario(Usuario usuario) {
-		usuarioDAO.delete(usuario);
+		usuarioRepository.delete(usuario);
 	}
 	
 }
